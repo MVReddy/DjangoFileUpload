69166eff9b437e71c3fb29506fa6f67f1550086b
Venkata Reddy M <venkatareddy.mulam@techmahindra.com>
2015-06-09 11:56:27 +0530

    File Upload with Djang0
    This is a simple application through which you can do login, logout,
    forgot password and file upload operations.

---
+++ file_upload/file_upload/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^myapp/', include('myapp.urls', namespace="myapp")),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^authentication/', include('login.urls')),
+    url(r'^authenticaion/logout', include('login.urls')),
+)
